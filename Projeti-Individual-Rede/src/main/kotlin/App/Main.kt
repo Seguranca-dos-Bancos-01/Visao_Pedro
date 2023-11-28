33cb2634893aa96770034cc4ca626388bbaa52c8
Pyellini <pedro.pinto@sptech.school>
2023-11-28 18:02:16 -0300

    finalizado

---
+++ Projeti-Individual-Rede/src/main/kotlin/App/Main.kt
@@ -0,0 +1,23 @@
+package app
+
+import Repositorio
+
+open class Main {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+
+            // 1. Conexão com o Banco de Dados
+            Conexao.jdbcTemplate // Inicializa a conexão
+            // 2. Criar uma instância de Repositorio e chamar seus métodos
+            val repositorio = Repositorio()
+            repositorio.iniciar()
+            // 3. Executar a função monitoramento() de codigoPy
+            codigoPy.monitoramento()
+
+
+
+            println("Execução concluída.")
+
+        }
+    }
+}
--- Projeti-Individual-Rede/src/main/kotlin/Executar.kt
+++ Projeti-Individual-Rede/src/main/kotlin/Executar.kt
@@ -1,14 +1,13 @@
+package app
+
+import Repositorio
+
 fun main() {
     // 1. Conexão com o Banco de Dados
     Conexao.jdbcTemplate // Inicializa a conexão
-
     // 2. Criar uma instância de Repositorio e chamar seus métodos
-
     val repositorio = Repositorio()
     repositorio.iniciar()
-    //repositorio.validarColaborador()
-
-
     // 3. Executar a função monitoramento() de codigoPy
     codigoPy.monitoramento()
 
